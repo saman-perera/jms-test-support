03c10be81dbe3276d99ba89a78e710c3f2257d99
ladislav.gazo <ladislav.gazo@5be1c10c-ff39-11dd-984d-ab748d24aa7e>
2009-02-23 14:19:39 +0000

    comments and documentation of functionality

--- trunk/src/main/java/sk/seges/test/jms/ProviderConfiguration.java
+++ trunk/src/main/java/sk/seges/test/jms/ProviderConfiguration.java
@@ -1,5 +1,37 @@
+/*
+ * Copyright 2009 Seges Ltd. 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ */
 package sk.seges.test.jms;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation for general provider behaviour per test suite.
+ * 
+ * @author ladislav.gazo
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface ProviderConfiguration {
+	/**
+	 * @return True when after each finished test case a JMS provider has to be
+	 *         restarted. False when provider will be only stopped after all
+	 *         test cases were finished.
+	 */
 	boolean restartAfterEachTest();
 }
--- trunk/src/main/java/sk/seges/test/jms/SpringJMSSuite.java
+++ trunk/src/main/java/sk/seges/test/jms/SpringJMSSuite.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2009 Seges Ltd. 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ */
 package sk.seges.test.jms;
 
 import java.util.List;
@@ -9,10 +24,22 @@ import org.springframework.test.context.ContextConfiguration;
 
 import sk.seges.test.jms.manager.JMSProviderManager;
 
+/**
+ * Extended JMS suite with support to configure all aspects using Spring
+ * framework.
+ * 
+ * Specific provider behaviour configuration is done using
+ * {@link SpringProviderConfiguration}. Default bean name for list of JMS
+ * provider managers is defined in {@link SpringJMSSuite.JMS_PROVIDER_MANAGERS} constant (currently it
+ * is "jmsProviderManagers").
+ * 
+ * @author ladislav.gazo
+ */
 public class SpringJMSSuite extends JMSSuite {
-	private static final String JMS_PROVIDER_MANAGERS = "jmsProviderManagers";
+	/** Default bean name for JMS provider managers. */
+	public static final String JMS_PROVIDER_MANAGERS = "jmsProviderManagers";
 	private ApplicationContext context;
-	
+
 	public SpringJMSSuite(Class<?> klass, Class<?>[] annotatedClasses)
 			throws InitializationError {
 		super(klass, annotatedClasses);
@@ -25,19 +52,22 @@ public class SpringJMSSuite extends JMSSuite {
 	@SuppressWarnings("unchecked")
 	@Override
 	protected void setManagers(Class<?> klass) {
-		ContextConfiguration annotation = klass.getAnnotation(ContextConfiguration.class);
-		if(annotation != null) {
+		ContextConfiguration annotation = klass
+				.getAnnotation(ContextConfiguration.class);
+		if (annotation != null) {
 			context = new ClassPathXmlApplicationContext(annotation.locations());
 		} else {
 			context = new ClassPathXmlApplicationContext("context.xml");
 		}
-		
-		SpringProviderConfiguration providerConfiguration = klass.getAnnotation(SpringProviderConfiguration.class);
-		if(providerConfiguration != null) {
-			managers = (List<JMSProviderManager>) context.getBean(providerConfiguration.managersListName());	
+
+		SpringProviderConfiguration providerConfiguration = klass
+				.getAnnotation(SpringProviderConfiguration.class);
+		if (providerConfiguration != null) {
+			managers = (List<JMSProviderManager>) context
+					.getBean(providerConfiguration.managersListName());
 		} else {
-			managers = (List<JMSProviderManager>) context.getBean(JMS_PROVIDER_MANAGERS);
+			managers = (List<JMSProviderManager>) context
+					.getBean(JMS_PROVIDER_MANAGERS);
 		}
 	}
-
 }
